58cb9f2729c1e12d2a8336fd0846b2d307d606b9
Vinnie <vin.zedigh@adelfors.nu>
2024-11-26 11:25:42 +0100

    followed comment stuff and made progress

--- ada5/Program.cs
+++ ada5/Program.cs
@@ -16,25 +16,34 @@ namespace ada5
 
 		static void Main(string[] args)
 		{
+			List<Dictionary<char, int>> TextSideList = new List<Dictionary<char, int>>();
+			List<Dictionary<char, int>> InputSideList = new List<Dictionary<char, int>>();
 
+			RegPhrase current = new(Console.ReadLine() ?? "");
+			MatchCollection matches = current.regex.Matches(pureText);
+			for (int i = 0; i < matches.Count(); i++)
+			{
+				string m = matches[i].ToString();
+				Dictionary<char, int> TextSide = new();
+				Dictionary<char, int> InputSide = new();
+				foreach (char c in m)
+				{
+					TextSide.TryAdd(c, m.Count(o => o == c));
+				}
+				string n = current.words[i];
+				foreach (char v in n)
+				{
+					InputSide.TryAdd(v, n.Count(p => p == v));
+				}
+				TextSideList.Add(TextSide);
+				InputSideList.Add(InputSide); //storing each word as a dictionary so that they can be processed later.
+			}
 		}
 		// I will try to convert user input to a regex, then I will search for matches in pureText, and compare the number of occurrences of each letter to see if it is a possible match.
 		// Maybe I can get it to remember the position index of each letter to see if the letters match, not just the numbers...
 		// If there is more than one possible match, it should say that and let the user choose to guess on one or skip.
 		// It should also check if any of the stored key letters are in the regex match, and display those to the user, maybe underneath with formatting to see the missing letters.
-		public static Regex StringToRegexCapped(string input)
-		{
-			string temp = input.ToUpper();
-
-			MatchCollection words = word.Matches(temp);
-
-			string regg = "";
-			foreach (Match m in words)
-			{
-				string _word = m.Value;
-
-			}
-		}
+		
 
 		public static string RemovePunctuation(string text)
 		{
---
+++ ada5/RegPhrase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ada5
+{
+	public class RegPhrase
+	{
+		static readonly string alfaUpper = @"[ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ]";
+		static readonly string alfaLower = @"[abcdefghijklmnopqrstuvwxyzåäö]";
+		static readonly string alfaAny = @"[ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖabcdefghijklmnopqrstuvwxyzåäö]";
+		static readonly string alfaNot = @"[^ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖabcdefghijklmnopqrstuvwxyzåäö]";
+		static readonly Regex regAlfaUpper = new(alfaUpper);
+		static readonly Regex regAlfaLower = new(alfaLower);
+		static readonly Regex regNotAlfa = new(alfaNot);
+		static readonly Regex regNum = new(@"\d");
+		public Regex regex { get; set; }
+		public string Phrase { get; set; }
+		public string[] words { get; set; }
+
+		public RegPhrase(string phrase)
+		{
+			Phrase = phrase.ToUpper();
+			regex = StringToRegexUpper(phrase);
+			words = Regex.Matches(phrase, alfaUpper).Cast<Match>().Select(m => m.Value).ToArray();
+		}
+
+
+		static readonly Regex word = new(@"\b" + alfaUpper + @"+" + alfaNot);
+
+		public static Regex StringToRegexUpper(string inputPhrase)
+		{
+			string temp = inputPhrase.ToUpper();
+			MatchCollection matches = word.Matches(temp);
+
+			string regg = @"";
+			for (int i = 0; i < matches.Count(); i++)
+			{
+				regg += alfaUpper + @"{" + matches[i].Length + @"}";
+				if (i != matches.Count() - 1)
+					regg += @" ";
+			}
+			return new(regg);
+		}
+	}
+}
